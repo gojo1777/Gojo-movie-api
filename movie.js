3fb61f6656f6b209cdfe3f6d7edbbeb42f4e5870
gojo1777 <gojomovie0@gmail.com>
2025-05-28 20:16:04 +0530

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,16 @@
+[
+  {
+    "id": 1,
+    "title": "Pathaan (2023)",
+    "subtitle": "සිංහල උපසිරැසි",
+    "link": "https://example.com/pathaan",
+    "poster": "https://example.com/pathaan.jpg"
+  },
+  {
+    "id": 2,
+    "title": "KGF Chapter 2",
+    "subtitle": "සිංහල උපසිරැසි",
+    "link": "https://example.com/kgf2",
+    "poster": "https://example.com/kgf2.jpg"
+  }
+]
